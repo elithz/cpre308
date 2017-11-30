3ad795741294064c34d63ec254e5342d931eb6ab
狮子劫博丽 <elithzhang@gmail.com>
2017-11-30 14:44:08 -0600

    small format change

--- lab7&8/bsdump-template.c
+++ lab7&8/bsdump-template.c
@@ -7,7 +7,6 @@
 *        Company:  NERVE Software
 */
 
- 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -15,97 +14,94 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
-#define SIZE 32  /* size of the read buffer */
+#define SIZE 32 /* size of the read buffer */
 //define PRINT_HEX // un-comment this to print the values in hex for debugging
 
 struct BootSector
 {
-    unsigned char  sName[9];            // The name of the volume
-    unsigned short iBytesSector;        // Bytes per Sector
-    
-    unsigned char  iSectorsCluster;     // Sectors per Cluster
-    unsigned short iReservedSectors;    // Reserved sectors
-    unsigned char  iNumberFATs;         // Number of FATs
-    
-    unsigned short iRootEntries;        // Number of Root Directory entries
-    unsigned short iLogicalSectors;     // Number of logical sectors
-    unsigned char  xMediumDescriptor;   // Medium descriptor
-    
-    unsigned short iSectorsFAT;         // Sectors per FAT
-    unsigned short iSectorsTrack;       // Sectors per Track
-    unsigned short iHeads;              // Number of heads
-    
-    unsigned short iHiddenSectors;      // Number of hidden sectors
-};
+	unsigned char sName[9];		 // The name of the volume
+	unsigned short iBytesSector; // Bytes per Sector
 
+	unsigned char iSectorsCluster;   // Sectors per Cluster
+	unsigned short iReservedSectors; // Reserved sectors
+	unsigned char iNumberFATs;		 // Number of FATs
+
+	unsigned short iRootEntries;	 // Number of Root Directory entries
+	unsigned short iLogicalSectors;  // Number of logical sectors
+	unsigned char xMediumDescriptor; // Medium descriptor
+
+	unsigned short iSectorsFAT;   // Sectors per FAT
+	unsigned short iSectorsTrack; // Sectors per Track
+	unsigned short iHeads;		  // Number of heads
+
+	unsigned short iHiddenSectors; // Number of hidden sectors
+};
 
 unsigned short endianSwap(unsigned char one, unsigned char two);
 // Pre: Two initialized characters
 // Post: The characters are swapped (two, one) and returned as a short
 
-void decodeBootSector(struct BootSector * pBootS, unsigned char buffer[]);
+void decodeBootSector(struct BootSector *pBootS, unsigned char buffer[]);
 // Pre: An initialized BootSector struct and a pointer to an array
 //      of characters read from a BootSector
 // Post: The BootSector struct is filled out from the buffer data
 
-void printBootSector(struct BootSector * pBootS);
+void printBootSector(struct BootSector *pBootS);
 // Pre: A filled BootSector struct
 // Post: The information about the boot sector prints to the console
 
-
 // entry point:
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
 	int pBootSector = 0;
 	unsigned char buffer[SIZE];
 	struct BootSector sector;
-    
+
 	// Check for argument
-	if (argc < 2) {
+	if (argc < 2)
+	{
 		printf("Specify boot sector\n");
 		exit(1);
 	}
-    
+
 	// Open the file and read the boot sector
 	pBootSector = open(argv[1], O_RDONLY);
 	read(pBootSector, buffer, SIZE);
-    close(pBootSector);
-    
+	close(pBootSector);
+
 	// Decode the boot Sector
 	decodeBootSector(&sector, buffer);
-    
+
 	// Print Boot Sector information
 	printBootSector(&sector);
-    
-    return 0;
-}  // end main()
 
+	return 0;
+} // end main()
 
 // Converts two characters to an unsigned short with two, one
 unsigned short endianSwap(unsigned char one, unsigned char two)
 {
-    // swap
+	// swap
 	return (two << 8) | one;
 }
 
-
 // Fills out the BootSector Struct from the buffer
-void decodeBootSector(struct BootSector * pBootS, unsigned char buffer[])
+void decodeBootSector(struct BootSector *pBootS, unsigned char buffer[])
 {
-	int i = 3;  // Skip the a 3 bytes
+	int i = 3; // Skip the a 3 bytes
 	unsigned char a;
 	unsigned char b;
-    
+
 	// Pull the name and put it in the struct (remember to null-terminate)
 	memcpy(pBootS->sName, &(buffer[i]), 8);
 	pBootS->sName[9] = '\0';
-    
+
 	// Read bytes/sector and convert to big endian
 	i = 11; //offset for bytes/sector
 	a = buffer[i++];
 	b = buffer[i++];
 	pBootS->iBytesSector = endianSwap(a, b);
-    
+
 	// Read sectors/cluster, Reserved sectors and Number of Fats
 	a = buffer[i++];
 	b = 0;
@@ -114,48 +110,47 @@ void decodeBootSector(struct BootSector * pBootS, unsigned char buffer[])
 	a = buffer[i++];
 	b = buffer[i++];
 	pBootS->iReservedSectors = endianSwap(a, b);
-	
+
 	a = buffer[i++];
 	b = 0;
 	pBootS->iNumberFATs = endianSwap(a, b);
-    
+
 	// Read root entries, logicical sectors and medium descriptor
 	a = buffer[i++];
 	b = buffer[i++];
 	pBootS->iRootEntries = endianSwap(a, b);
-	
+
 	a = buffer[i++];
 	b = buffer[i++];
 	pBootS->iLogicalSectors = endianSwap(a, b);
-	
+
 	a = buffer[i++];
 	b = 0;
-	pBootS->xMediumDescriptor = endianSwap(a, b);	
-    
+	pBootS->xMediumDescriptor = endianSwap(a, b);
+
 	// Read and covert sectors/fat, sectors/track, and number of heads
 	a = buffer[i++];
 	b = buffer[i++];
 	pBootS->iSectorsFAT = endianSwap(a, b);
-	
+
 	a = buffer[i++];
 	b = buffer[i++];
 	pBootS->iSectorsTrack = endianSwap(a, b);
-	
+
 	a = buffer[i++];
 	b = buffer[i++];
 	pBootS->iHeads = endianSwap(a, b);
-    
+
 	// Read hidden sectors
 	a = buffer[i++];
 	b = buffer[i++];
 	pBootS->iHiddenSectors = endianSwap(a, b);
-    
+
 	return;
 }
 
-
 // Displays the BootSector information to the console
-void printBootSector(struct BootSector * pBootS)
+void printBootSector(struct BootSector *pBootS)
 {
 #ifndef PRINT_HEX
 	printf("                    Name:   %s\n", pBootS->sName);
@@ -171,7 +166,7 @@ void printBootSector(struct BootSector * pBootS)
 	printf("         Number of heads:   %i\n", pBootS->iHeads);
 	printf("Number of Hidden Sectors:   %i\n", pBootS->iHiddenSectors);
 #else
-	printf("                    Name:   %s\n",     pBootS->sName);
+	printf("                    Name:   %s\n", pBootS->sName);
 	printf("            Bytes/Sector:   0x%04x\n", pBootS->iBytesSector);
 	printf("         Sectors/Cluster:   0x%02x\n", pBootS->iSectorsCluster);
 	printf("        Reserved Sectors:   0x%04x\n", pBootS->iReservedSectors);
